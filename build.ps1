311b85c9d71425fb1bf3b2dcf983a81533d0a2db
Silvio Heuberger <silvio.heuberger@gmail.com>
2024-09-09 14:20:32 +0200

    Recursive copy the directories

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ pip install pyinstaller
 pip install -r requirements.txt
 
 pyinstaller --onefile -w .\FalconShaker.py
-cp .\Profiles\ .\dist\
-cp '.\Sound Files\' .\dist\
+cp -r .\Profiles\ .\dist\
+cp -r '.\Sound Files\' .\dist\
 
 Compress-Archive -Update .\dist\ falcon-shaker.zip
